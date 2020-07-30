640368056cb0c900cfe773bdde8d60323685577c
LouetR <romainlouet@romsworld.net>
2020-07-30 12:42:21 +0200

    Create HTTP Request and Response class

--- rlfram/Core/HTTPResponse.php
+++ rlfram/Core/HTTPResponse.php
@@ -4,7 +4,42 @@
 namespace App\Core;
 
 
-class HTTPResponse
+class HTTPResponse extends ApplicationComponent
 {
+    protected $page;
 
+    public function addHeader($header)
+    {
+        $header($header);
+    }
+
+    public function redirect($location)
+    {
+        header('location: '.$location);
+        exit;
+    }
+
+    public function redirect404()
+    {
+
+    }
+
+    public function send(){
+        // TODO
+
+        exit($this->page->getGeneratedPage());
+    }
+
+    /**
+     * @param Page $page
+     */
+    public function setPage(Page $page)
+    {
+        $this->page = $page;
+    }
+
+    public function setCookie($name, $value = '', $expire = 0, $path = null, $domain = null, $secure = false, $httpOnly = true)
+    {
+        $this->setCookie($name, $value, $expire, $path, $domain, $secure, $httpOnly);
+    }
 }
