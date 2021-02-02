816951dfd770c3527294b06bc7c139afba8e88a5
LouetR <romainlouet@romsworld.net>
2021-02-02 22:48:45 +0100

    improve custom-alerts.js code

--- public/custom/js/custom-alerts.js
+++ public/custom/js/custom-alerts.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
-    $('.alert', $('#flashMessages')).each(function () {
+    $(".alert", $("#flashMessages")).each(function () {
         let $alertContainer = $(this);
         setTimeout(function(){
-            $alertContainer.alert('close');
+            $alertContainer.alert("close");
         }, 15000);
     });
 });
 
 function showFlashMessage(type, message) {
-    var $flashContainer = $('#flashMessages');
-    let $flashAlert = $('<div class="alert alert-' + type + ' alert-dismissible fade show" role="alert">\n' +
-        '            <button type="button" class="close" data-dismiss="alert" aria-label="Fermer">\n' +
-        '                <span aria-hidden="true"><i class="funky-ui-icon icon-Close"></i></span>\n' +
-        '            </button>\n' +
-        '            <div>' + message + '</div>\n' +
-        '        </div>');
+    let $flashContainer = $("#flashMessages");
+    let $flashAlert = $("<div class=\"alert alert-" + type + " alert-dismissible fade show\" role=\"alert\">\n" +
+        "            <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Fermer\">\n" +
+        "                <span aria-hidden=\"true\"><i class=\"funky-ui-icon icon-Close\"></i></span>\n" +
+        "            </button>\n" +
+        "            <div>" + message + "</div>\n" +
+        "        </div>");
     $flashContainer.append($flashAlert);
     setTimeout(function(){
-        $flashAlert.alert('close');
+        $flashAlert.alert("close");
     }, 10000);
 }
