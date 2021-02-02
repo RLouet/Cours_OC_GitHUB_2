4f412e2b9da8f2626bd5cda4d42f767f3c62e15a
LouetR <romainlouet@romsworld.net>
2021-02-02 22:02:47 +0100

    improve custom-profile-password.js code

--- public/custom/js/custom-profile-password.js
+++ public/custom/js/custom-profile-password.js
@@ -1,68 +1,66 @@
 $(document).ready(function() {
-	$('#passwordModal form').submit(function (e) {
+	$("#passwordModal form").submit(function (e) {
 		e.preventDefault();
 		let $form = $(this);
-		let formAction = $(this).attr('action');
-		$('.form-error', $(this)).addClass('hidden');
+		let formAction = $(this).attr("action");
+		$(".form-error", $(this)).addClass("hidden");
 		let formData = $form.serialize();
 
-		let oldPassword = $('.pw-old input', $(this)).val();
-		let newPassword = $('.pw-new input', $(this)).val();
-		let confPassword = $('.pw-conf input', $(this)).val();
+		let oldPassword = $(".pw-old input", $(this)).val();
+		let newPassword = $(".pw-new input", $(this)).val();
+		let confPassword = $(".pw-conf input", $(this)).val();
 
 		if (newPassword !== confPassword) {
-			$('input.form-control', $(this)).val("");
-			$('.pw-conf .form-error', $form).removeClass('hidden');
-			return
+			$("input.form-control", $(this)).val("");
+			$(".pw-conf .form-error", $form).removeClass('hidden');
+			return;
 		}
 
-		//alert (formData);
 		$.ajax({
 			url: formAction,
 			method: "POST",
 			data: formData,
 			dataType: "json",
-			success: function (data) {
+			success(data) {
 				if (!data.success){
 					if (data.token_error) {
 						document.location.reload();
 					}
 					if (data.old_error) {
-						$('.pw-old .form-error', $form).removeClass('hidden');
+						$(".pw-old .form-error", $form).removeClass("hidden");
 					}
 					if (data.new_error) {
-						$('.pw-new .form-error', $form).removeClass('hidden');
+						$(".pw-new .form-error", $form).removeClass("hidden");
 					}
 					if (data.conf_error) {
-						$('.pw-conf .form-error', $form).removeClass('hidden');
+						$(".pw-conf .form-error", $form).removeClass("hidden");
 					}
 					if (data.user_error) {
-						$('.pw-general-error .form-error span', $form).html('Votre profil est invalide');
-						$('.pw-general-error .form-error', $form).removeClass('hidden');
+						$(".pw-general-error .form-error span", $form).html("Votre profil est invalide");
+						$(".pw-general-error .form-error", $form).removeClass("hidden");
 					}
 					if (data.db_error) {
-						$('.pw-general-error .form-error span', $form).html("Erreur lors de l'enregistrement");
-						$('.pw-general-error .form-error', $form).removeClass('hidden');
+						$(".pw-general-error .form-error span", $form).html("Erreur lors de l'enregistrement");
+						$(".pw-general-error .form-error", $form).removeClass("hidden");
 					}
-					$('input.form-control', $(this)).val("");
+					$("input.form-control", $(this)).val("");
 				} else {
-					$('#passwordModal').modal('hide');
+					$('#passwordModal').modal("hide");
 
-					showFlashMessage('success', 'Votre mot de passe a bien été modifié')
+					showFlashMessage("success", "Votre mot de passe a bien été modifié");
 				}
 			},
-			error: function (e) {
-				//alert('ajax');
-				$('.sk-general-error .form-error span', $form).html('Erreur Ajax');
-				$('.sk-general-error .form-error', $form).removeClass('hidden');
+			error(e) {
+				$(".sk-general-error .form-error span", $form).html("Erreur Ajax");
+				$(".sk-general-error .form-error", $form).removeClass("hidden");
 			}
-		})
+		});
 	});
 
-	$('#passwordModal').on('hide.bs.modal', function(event) {
-		$('input.form-control', this).val('');
+	$("#passwordModal").on("hide.bs.modal", function(event) {
+		$("input.form-control", this).val("");
 
-		$('.form-error', $(this)).addClass('hidden');
+		$(".form-error", $(this)).addClass("hidden");
 	});
 
-})
+});
