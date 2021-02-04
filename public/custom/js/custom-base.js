514c0652fbb11c52028c7883e7272949e9e01983
LouetR <romainlouet@romsworld.net>
2021-02-04 14:20:23 +0100

    improve custo-base.js code

--- public/custom/js/custom-base.js
+++ public/custom/js/custom-base.js
@@ -1,84 +1,80 @@
-let customBase = (function($) {
-	"use strict";
+/*global Royal_Preloader*/
 
-	//Preloader
-	Royal_Preloader.config({
-		mode: 'progress',
-		background: '#aaaaaa',
-		showProgress: true,
-		showPercentage: false
+Royal_Preloader.config({
+	mode: "progress",
+	background: "#aaaaaa",
+	showProgress: true,
+	showPercentage: false
+});
+
+//Page Scroll to id
+$(window).on("load",function(){
+
+	/* Page Scroll to id fn call */
+	$("a.nav-link,a[href='#top'],a[data-gal='m_PageScroll2id']").mPageScroll2id({
+		highlightSelector:"a.nav-link",
+		offset: 67,
+		scrollSpeed:800
 	});
 
-	//Page Scroll to id
-	$(window).on("load",function(){
-
-		/* Page Scroll to id fn call */
-		$("a.nav-link,a[href='#top'],a[data-gal='m_PageScroll2id']").mPageScroll2id({
-			highlightSelector:"a.nav-link",
-			offset: 67,
-			scrollSpeed:800
-		});
-
-		/* demo functions */
-		$("a[rel='next']").click(function(e){
-			e.preventDefault();
-			var to=$(this).parent().parent("section").next().attr("id");
-			$.mPageScroll2id("scrollTo",to);
-		});
-
+	/* demo functions */
+	$("a[rel='next']").click(function(e){
+		e.preventDefault();
+		var to=$(this).parent().parent("section").next().attr("id");
+		$.mPageScroll2id("scrollTo",to);
 	});
 
-	//Parallax & fade on scroll
-	function scrollBanner() {
-		$(document).scroll(function(){
-			var scrollPos = $(this).scrollTop();
-			$('.parallax-fade-top').css({
-				'top' : (scrollPos/2)+'px',
-				'opacity' : 1-(scrollPos/750)
-			});
+});
+
+//Parallax & fade on scroll
+function scrollBanner() {
+	$(document).scroll(function(){
+		var scrollPos = $(this).scrollTop();
+		$('.parallax-fade-top').css({
+			top : (scrollPos/2)+'px',
+			opacity : 1-(scrollPos/750)
 		});
-	}
-	scrollBanner();
+	});
+}
+scrollBanner();
 
 
-	$(document).ready(function() {
+$(document).ready(function() {
 
 
-		$(".tipped").tipper();
+	$(".tipped").tipper();
 
-		/* Scroll Too */
-		$(".scroll").on('click', function(event){
+	/* Scroll Too */
+	$(".scroll").on("click", function(event){
 
-			event.preventDefault();
+		event.preventDefault();
 
-			var full_url = this.href;
-			var parts = full_url.split("#");
-			var trgt = parts[1];
-			var target_offset = $("#"+trgt).offset();
-			var target_top = target_offset.top - 68;
+		var fullUrl = this.href;
+		var parts = fullUrl.split("#");
+		var trgt = parts[1];
+		var target_offset = $("#"+trgt).offset();
+		var targetTop = target_offset.top - 68;
 
-			$('html, body').animate({scrollTop:target_top}, 800);
-		});
-
-		//Scroll back to top
-		var offset = 300;
-		var duration = 600;
-		jQuery(window).on('scroll', function() {
-			if (jQuery(this).scrollTop() > offset) {
-				jQuery('.scroll-to-top').fadeIn(duration);
-			} else {
-				jQuery('.scroll-to-top').fadeOut(duration);
-			}
-		});
-
-		jQuery('.scroll-to-top').on('click', function(event) {
-			event.preventDefault();
-			jQuery('html, body').animate({scrollTop: 0}, duration);
-			return false;
-		});
-
-		//Parallax
-		$('.parallax').parallax("50%", 0.3);
+		$("html, body").animate({scrollTop:targetTop}, 800);
 	});
 
-})(jQuery);
+	//Scroll back to top
+	var offset = 300;
+	var duration = 600;
+	jQuery(window).on("scroll", function() {
+		if (jQuery(this).scrollTop() > offset) {
+			jQuery(".scroll-to-top").fadeIn(duration);
+		} else {
+			jQuery(".scroll-to-top").fadeOut(duration);
+		}
+	});
+
+	jQuery(".scroll-to-top").on("click", function(event) {
+		event.preventDefault();
+		jQuery("html, body").animate({scrollTop: 0}, duration);
+		return false;
+	});
+
+	//Parallax
+	$(".parallax").parallax("50%", 0.3);
+});
